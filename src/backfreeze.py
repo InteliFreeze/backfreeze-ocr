d898c2639e7a0ed87cd63912184214e7c1b2ffae
Vitor Sasaki <vsasakiv@usp.br>
2022-07-07 21:07:50 -0300

    improved image processing

--- src/backfreeze.py
+++ src/backfreeze.py
@@ -1,4 +1,3 @@
-from cv2 import imread
 from fastapi import FastAPI
 from pydantic import BaseModel
 from pyzbar import pyzbar
@@ -14,14 +13,28 @@ def read_image(base64_image):
     # Convert the image to grayscale a
     imgdata = base64.b64decode(base64_image)
     img = PIL.Image.open(io.BytesIO(imgdata))
+
     img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+
     img = cv2.resize(img, None, fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
+
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    kernel = np.ones((1,1), np.uint8)
+    se=cv2.getStructuringElement(cv2.MORPH_RECT , (8,8))
+    bg=cv2.morphologyEx(img, cv2.MORPH_DILATE, se)
+    out_gray=cv2.divide(img, bg, scale=255)
+    img = cv2.threshold(out_gray, 0, 255, cv2.THRESH_OTSU )[1] 
+
+
+    kernel = np.ones((3,3), np.uint8)
     img = cv2.dilate(img, kernel, iterations=1)
-    img = cv2.erode(img, kernel, iterations=1)
-    cv2.adaptiveThreshold(cv2.medianBlur(img, 3), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
-    text = pytesseract.image_to_string(img)
+    img = cv2.erode(img, kernel, iterations=4)
+    #img = cv2.fastNlMeansDenoising(img, None, 20, 7, 21) 
+    #img = cv2.erode(img, kernel, iterations=2)
+    
+    text = pytesseract.image_to_string(img, config="-c tessedit_char_whitelist=0123456789./-")
+    text = text.strip()
+    text = text.replace(" ", "")
+    print(text)
     return text
 
 def read_barcode(base64_image):
